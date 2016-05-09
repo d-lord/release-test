0b7f2bcd98643d623c186d5e27d81acde763d82c
David Lord <eternalcat@gmail.com>
2016-05-09 13:13:37 +1000

    Port random() to iOS

--- ios.c
+++ ios.c
@@ -9,3 +9,7 @@ uintptr_t b32decode(char* s) {
 	return (s)? *s : (uintptr_t) s;
 	// TODO: small chance of overflowing on systems with more than 256 cells of memory
 }
+
+int random() {
+	return 4;
+}
