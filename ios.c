ae03dd139506f00b875e0709af6e40af5517250b
David Lord <eternalcat@gmail.com>
2016-05-09 12:39:33 +1000

    Implement b32decode for ios

--- ios.c
+++ ios.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
+#include <math.h>
 
 void smart() {
 	printf("Hello world!");
+}
+
+uint8_t b32decode(char* s) {
+	return (s)? *s : s;
+	// TODO: small chance of overflowing on systems with more than 256 cells of memory
 }
